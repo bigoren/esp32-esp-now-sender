b573594077bb413757965f50b59204af2a64ad29
Oren Bigler <bigoreo@gmail.com>
2024-06-17 21:17:36 +0300

    Changing sender to control lum by IO state

--- src/sender.cpp
+++ src/sender.cpp
@@ -2,7 +2,7 @@
  * "THE BEER-WARE LICENSE" (Revision 42):
  * regenbogencode@gmail.com wrote this file. As long as you retain this notice
  * you can do whatever you want with this stuff. If we meet some day, and you
- * think this stuff is worth it, you can buy me a beer in return
+ * think this stuff is worth it, you can buy me lum beer in return
  */
 #include <Arduino.h>
 #ifdef ESP8266
@@ -12,12 +12,17 @@
 #endif
 #include "ESPNowW.h"
 
+#define CONTROL_INPUT_PIN 2
+#define MAX_LUM 30
+#define MIN_LUM 0
+
 // uint8_t receiver_mac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
-uint8_t receiver_mac[] = {0x40, 0x22, 0xD8, 0x5E, 0x6D, 0x04};
+// uint8_t receiver_mac[] = {0x40, 0x22, 0xD8, 0x5E, 0x6D, 0x04};
+uint8_t receiver_mac[] = {0x08, 0xB6, 0x1F, 0xB9, 0x2B, 0xE4};
 
 void setup() {
     Serial.begin(115200);
-    Serial.println("ESPNow sender Demo");
+    Serial.println("ESPNow sender started.");
 #ifdef ESP8266
     WiFi.mode(WIFI_STA); // MUST NOT BE WIFI_MODE_NULL
 #elif ESP32
@@ -26,11 +31,39 @@ void setup() {
     WiFi.disconnect();
     ESPNow.init();
     ESPNow.add_peer(receiver_mac);
+
+    pinMode(CONTROL_INPUT_PIN, INPUT);
+    Serial.println("Control pin number is: " + String(CONTROL_INPUT_PIN));
 }
 
+int dir = 0;
+bool state = LOW;
+bool prevState = LOW;
+static uint8_t lum = 0;
+
 void loop() {
-    static uint8_t a = 0;
-    delay(1000);
-    ESPNow.send_message(receiver_mac, &a, 1);
-    Serial.println(a++);
+    state = digitalRead(CONTROL_INPUT_PIN); // read state from control pin
+    // Set direction when state changes
+    if ((state == HIGH) && (prevState == LOW)) {
+        dir = 1;
+        Serial.println("State change from LOW to HIGH");
+    } else if ((state == LOW) && (prevState == HIGH)) {
+        dir = -1;
+        Serial.println("State change from HIGH to LOW");
+    }
+    // Increase or decrease lum value
+    lum += dir;
+    // Stop when lum is out of bounds
+    if ((lum > MAX_LUM) || (lum <= MIN_LUM)) {
+        dir = 0;
+    // Send lum as it changes
+    } else {
+        ESPNow.send_message(receiver_mac, &lum, 1);
+        Serial.println(lum);
+    }
+    // Update previous state
+    prevState = state;
+
+    // Delay determines fade speed
+    delay(100);
 }
