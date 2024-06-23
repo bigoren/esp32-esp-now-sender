ba38f7bb0bf4ea1531a647a9d8c9f53a71566877
Oren Bigler <bigoreo@gmail.com>
2024-06-23 18:39:25 +0300

    Adding events and start IO

--- src/sender.cpp
+++ src/sender.cpp
@@ -12,13 +12,28 @@
 #endif
 #include "ESPNowW.h"
 
-#define CONTROL_INPUT_PIN 2
-#define MAX_LUM 30
-#define MIN_LUM 0
+#define START_INPUT_PIN 2
+#define QUE_INPUT_PIN 4
+#define MAX_LUM 35
+#define MIN_LUM 1
+#define MAX_EVENT_CNT 15
 
 // uint8_t receiver_mac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
 // uint8_t receiver_mac[] = {0x40, 0x22, 0xD8, 0x5E, 0x6D, 0x04};
 uint8_t receiver_mac[] = {0x08, 0xB6, 0x1F, 0xB9, 0x2B, 0xE4};
+
+bool eventFlag = false;
+bool start = LOW;
+bool state = LOW;
+bool prevState = LOW;
+uint8_t eventCount = 0;
+uint8_t data[2];
+uint8_t lum = MIN_LUM;
+uint8_t delayTime = 1;
+
+// Monitoring
+unsigned long lastMonitorTime = 0;
+unsigned long MonitorDelay = 1000;
 
 void setup() {
     Serial.begin(115200);
@@ -32,38 +47,108 @@ void setup() {
     ESPNow.init();
     ESPNow.add_peer(receiver_mac);
 
-    pinMode(CONTROL_INPUT_PIN, INPUT);
-    Serial.println("Control pin number is: " + String(CONTROL_INPUT_PIN));
+    pinMode(START_INPUT_PIN, INPUT);
+    pinMode(QUE_INPUT_PIN, INPUT);
+    Serial.println("Start pin number is: " + String(START_INPUT_PIN));
+    Serial.println("Que pin number is: " + String(QUE_INPUT_PIN));
+
+    ESPNow.send_message(receiver_mac, data, 2);
+    Serial.println("Initial lum is: " + String(lum));
+    Serial.println("Default delay is: " + String(delayTime));
 }
 
-int dir = 0;
-bool state = LOW;
-bool prevState = LOW;
-static uint8_t lum = 0;
+// delayTime calculation is dependent on the dimmer resolution, at 13 bits (2^13=8192) 1% is ~82 steps, so:
+// delayTime = (duration / percent_change) / 82
+// example: for 60 seconds and 34% change, (60 / 34) / 82 = 21 milliseconds
 
 void loop() {
-    state = digitalRead(CONTROL_INPUT_PIN); // read state from control pin
+    start = digitalRead(START_INPUT_PIN); // read state from start pin
+
+    state = digitalRead(QUE_INPUT_PIN); // read state from que pin
     // Set direction when state changes
     if ((state == HIGH) && (prevState == LOW)) {
-        dir = 1;
+        eventFlag = true;
+        eventCount++;
         Serial.println("State change from LOW to HIGH");
+        if (eventCount == 1) {
+            lum = MAX_LUM;
+            delayTime = 21; // for 59 secs and 34% change
+        } else if (eventCount == 3) {
+            lum = MAX_LUM;
+            delayTime = 19; // for 53 secs and 34% change
+        } else if (eventCount == 5) {
+            lum = MAX_LUM;
+            delayTime = 21;
+        } else if (eventCount == 7) {
+            lum = MAX_LUM;
+            delayTime = 19;
+        } else if (eventCount == 9) {
+            lum = MAX_LUM;
+            delayTime = 25; // for 70 secs and 34% change
+        } else if (eventCount == 11) {
+            lum = MAX_LUM;
+            delayTime = 16; // for 45 secs and 34% change 
+        } else if (eventCount == 13) {
+            lum = MAX_LUM;
+            delayTime = 23; // for 64 secs and 34% change
+        }
     } else if ((state == LOW) && (prevState == HIGH)) {
-        dir = -1;
+        eventFlag = true;
+        eventCount++;
         Serial.println("State change from HIGH to LOW");
+        if (eventCount == 2) {
+            lum = MIN_LUM;
+            delayTime = 27; // for 76 secs and 34% change
+        } else if (eventCount == 4) {
+            lum = MIN_LUM;
+            delayTime = 27;
+        } else if (eventCount == 6) {
+            lum = MIN_LUM;
+            delayTime = 35; // for 100 secs and 34% change
+        } else if (eventCount == 8) {
+            lum = MIN_LUM;
+            delayTime = 35;
+        } else if (eventCount == 10) {
+            lum = MIN_LUM;
+            delayTime = 21;
+        } else if (eventCount == 12) {
+            lum = MIN_LUM;
+            delayTime = 13; // for 37 secs and 34% change
+        } else if (eventCount == 14) {
+            lum = MIN_LUM;
+            delayTime = 30; // for 85 secs and 34% change
+        }
     }
-    // Increase or decrease lum value
-    lum += dir;
-    // Stop when lum is out of bounds
-    if ((lum > MAX_LUM) || (lum <= MIN_LUM)) {
-        dir = 0;
-    // Send lum as it changes
-    } else {
-        ESPNow.send_message(receiver_mac, &lum, 1);
-        Serial.println(lum);
+    // Send data at eventFlag
+    if (eventFlag) {
+        eventFlag = false;
+        data[0] = lum;
+        data[1] = delayTime;
+        ESPNow.send_message(receiver_mac, data, 2);
+        Serial.println("sent lum is: " + String(lum));
+        Serial.println("sent delay is: " + String(delayTime));
+        Serial.println("Event count: " + String(eventCount));
     }
+
+    if ((eventCount >= MAX_EVENT_CNT) || (start == HIGH)) {
+        Serial.println("Event count reached maximum or start IO is high. restarting.");
+        eventCount = 0;
+        data[0] = MIN_LUM;
+        data[1] = 1;
+        ESPNow.send_message(receiver_mac, data, 2);
+        Serial.println("sent lum is: " + String(MIN_LUM));
+        Serial.println("sent delay is: " + String(1));
+        Serial.println("Event count: " + String(eventCount));
+    }
+
     // Update previous state
     prevState = state;
 
-    // Delay determines fade speed
-    delay(100);
+    // Monitoring, not a must, just for debugging
+    if((millis() - lastMonitorTime) > MonitorDelay) {
+        Serial.print("ESP Now sender event count: "); Serial.println(eventCount);
+        lastMonitorTime = millis();
+    }
+    // Delay to slow things down
+    delay(1);
 }
